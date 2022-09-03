60c6358e3670243f8309f796b695ea9f8e8cc084
Gongmeda <gongmeda@gmail.com>
2022-09-03 21:07:44 +0900

    feat: 필요한 entity 추가

---
+++ src/main/java/org/hothyojas/mogayobackend/entities/Delivery.java
@@ -0,0 +1,37 @@
+package org.hothyojas.mogayobackend.entities;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String answer;
+
+    @Column
+    private boolean isResponsed;
+
+    @Column(updatable = false, nullable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/org/hothyojas/mogayobackend/entities/Parent.java
@@ -0,0 +1,38 @@
+package org.hothyojas.mogayobackend.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Parent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String inviteCode;
+
+    @Column
+    private int useCount;
+}
