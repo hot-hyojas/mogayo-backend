f15d361956950ac6bf204362cfaeeaa3d2cdf697
Gongmeda <gongmeda@gmail.com>
2022-09-03 20:05:30 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/org/hothyojas/mogayobackend/MogayoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.hothyojas.mogayobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogayoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MogayoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hothyojas/mogayobackend/MogayoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hothyojas.mogayobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogayoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
